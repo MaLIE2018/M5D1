f64be2c80d658359a1d90ee5fff9251652ee377c
MaLIE2018 <liebsch@pm.me>
2021-05-17 21:45:56 +0200

    Initial server

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send("At home directory")
+  res.render('index', { title: 'Express' });
+});
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+
+module.exports = router;
